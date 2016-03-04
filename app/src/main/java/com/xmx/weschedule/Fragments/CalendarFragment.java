4d83f3e31bc5dd0f534ae96c8c7a3691b7dd7a01
The-_-onE <834489218@qq.com>
2016-03-04 20:27:33 +0800

    Import DatePicker to my Project
    https://github.com/AigeStudio/DatePicker

--- app/src/main/java/com/xmx/weschedule/Fragments/CalendarFragment.java
+++ app/src/main/java/com/xmx/weschedule/Fragments/CalendarFragment.java
@@ -5,8 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.xmx.weschedule.Datepicker.cons.DPMode;
+import com.xmx.weschedule.Datepicker.views.DatePicker;
 import com.xmx.weschedule.R;
+
+import java.util.Calendar;
 
 /**
  *
@@ -17,7 +22,16 @@ public class CalendarFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
 
-
+        DatePicker picker = (DatePicker) view.findViewById(R.id.date_picker);
+        Calendar calendar = Calendar.getInstance();
+        picker.setDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1);
+        picker.setMode(DPMode.SINGLE);
+        picker.setOnDatePickedListener(new DatePicker.OnDatePickedListener() {
+            @Override
+            public void onDatePicked(String date) {
+                Toast.makeText(getContext(), date, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
